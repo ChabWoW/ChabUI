81895928c85ac4cd4402dadb4cb8db6f55111e4c
ChabWoW <paterakisnicolas@me.com>
2026-01-27 20:41:47 -0500

    Test commit

---
+++ Modules/SlashWA.lua
@@ -0,0 +1,17 @@
+local function ShowCooldownViewerSettings()
+	if InCombatLockdown() then
+		print("Try again once you're out of combat")
+		return
+	end
+
+	if not CooldownViewerSettings:IsShown() then
+		CooldownViewerSettings:Show()
+	else
+		CooldownViewerSettings:Hide()
+	end
+end
+
+SLASH_CDMSC1, SLASH_CDMSC2 = "/cd", "/wa"
+function SlashCmdList.CDMSC(msg, editbox)
+	ShowCooldownViewerSettings()
+end
