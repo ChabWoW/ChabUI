ac85f905223c4015fd74d4049148cbedc613eed5
Chab <paterakisnicolas@me.com>
2026-01-27 20:27:39 -0500

    Add files via upload

---
+++ ChabUI/Modules/SharedMedia_Chab/MyMedia.lua
@@ -0,0 +1,35 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+
+-- ----- 
+--  BORDER 
+-- ---- 
+LSM:Register("border", "Naowh1", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\border\Naowh1.tga]]) 
+LSM:Register("border", "Naowh2", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\border\Naowh2.tga]]) 
+LSM:Register("border", "Naowh3", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\border\Naowh3.tga]]) 
+LSM:Register("border", "roth", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\border\roth.tga]]) 
+LSM:Register("border", "SeerahScalloped", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\border\SeerahScalloped.tga]]) 
+
+-- -----
+--   FONT
+-- -----
+LSM:Register("font", "GothamNarrowUltra", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\font\GothamNarrowUltra.ttf]]) 
+LSM:Register("font", "KMT-GothamXN_Ultra", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\font\KMT-GothamXN_Ultra.ttf]]) 
+LSM:Register("font", "Naowh", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\font\Naowh.ttf]]) 
+
+-- -----
+--   SOUND
+-- -----
+
+-- -----
+--   STATUSBAR
+-- -----
+LSM:Register("statusbar", "blizzcast", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\statusbar\blizzcast.tga]]) 
+LSM:Register("statusbar", "blizzcast2", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\statusbar\blizzcast2.tga]]) 
+LSM:Register("statusbar", "blizzcastchannel", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\statusbar\blizzcastchannel.tga]]) 
+LSM:Register("statusbar", "blizzcastgrey", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\statusbar\blizzcastgrey.tga]]) 
+LSM:Register("statusbar", "blizzcastnonbreakable", [[Interface\AddOns\ChabUI\Modules\SharedMedia_Chab\statusbar\blizzcastnonbreakable.tga]]) 
