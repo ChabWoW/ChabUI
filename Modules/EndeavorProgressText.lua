23f353153046e4e12e5a196f48b156d5ff9b8e25
ChabWoW <paterakisnicolas@me.com>
2026-01-28 13:11:04 -0500

    Fixed Endeavor progress text

--- Modules/EndeavorProgressText.lua
+++ Modules/EndeavorProgressText.lua
@@ -1,10 +1,18 @@
-local frame = HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar
-
-if frame then
-    frame:HookScript("OnEnter", function()
-        HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar.TextContainer.ProgressText:Show()
+local frame = CreateFrame("Frame")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:SetScript("OnEvent", function()
+    EventRegistry:RegisterCallback("HousingDashboard.Toggled", function(isShown)
+        if isShown then
+            -- Your code here when HousingDashboardFrame shows
+            local progressBar = HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar
+            if progressBar then
+                    progressBar:HookScript("OnEnter", function()
+                        HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar.TextContainer.ProgressText:Show()
+                    end)
+                    progressBar:HookScript("OnLeave", function()
+                        HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar.TextContainer.ProgressText:Hide()
+                    end)
+                end
+        end
     end)
-    frame:HookScript("OnLeave", function()
-        HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar.TextContainer.ProgressText:Hide()
-    end)
-end
+end)
