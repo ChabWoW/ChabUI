bcfbe55fd1d415dc854959058f6830e53a6acdf8
ChabWoW <paterakisnicolas@me.com>
2026-01-28 12:50:53 -0500

    Disable endeavor text for now

--- Modules/EndeavorProgressText.lua
+++ Modules/EndeavorProgressText.lua
@@ -1,5 +1,10 @@
-local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:SetScript("OnEvent", function()
-    HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar.TextContainer.ProgressText:Show()
-end)
+local frame = HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar
+
+if frame then
+    frame:HookScript("OnEnter", function()
+        HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar.TextContainer.ProgressText:Show()
+    end)
+    frame:HookScript("OnLeave", function()
+        HousingDashboardFrame.HouseInfoContent.ContentFrame.InitiativesFrame.InitiativeSetFrame.ProgressBar.TextContainer.ProgressText:Hide()
+    end)
+end
